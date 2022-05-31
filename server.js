793a5db0f049b54fa80f80790d03d898d04a3b93
jamesfill <jamesp.fillerup@gmail.com>
2022-05-30 23:49:33 -0600

    menu questions

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 require('dotenv').config();
-
+const listOptions = ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add a employee', 'Update employee role'];
 
 //connect to database mysql
 const db = mysql.createConnection(
@@ -18,7 +18,14 @@ const db = mysql.createConnection(
     console.log('Connected to the employee database')
 );
 
-
+const questionMenu = [
+  {
+    type: 'table',
+    name: 'options',
+    message: 'What would you like to do?',
+    choices: listOptions
+  }
+]
 
 
 
