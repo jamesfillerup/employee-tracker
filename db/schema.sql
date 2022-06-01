2ce1b5bcbbf2007e07bcc58bf4386011c587cc81
jamesfill <jamesp.fillerup@gmail.com>
2022-06-01 11:16:54 -0600

    err duplicate data

--- db/schema.sql
+++ db/schema.sql
@@ -1,20 +1,21 @@
+DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
     CREATE TABLE department (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        id INTEGER PRIMARY KEY ,
         name VARCHAR(30) NOT NULL
     );
 
     CREATE TABLE role (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        id INTEGER PRIMARY KEY ,
         title VARCHAR(30) NOT NULL, 
         salary DECIMAL NOT NULL,
         department_id INTEGER
     );
 
     CREATE TABLE employee (
-        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+        id INTEGER PRIMARY KEY ,
         first_name VARCHAR(30) NOT NULL,
         last_name VARCHAR(30) NOT NULL,
         role_id INTEGER, 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,29 @@
-INSERT INTO department ( name)
+INSERT INTO department (  name)
 VALUES
-    ('Purchasing'),
-    ('Production'),
-    ('Marketing');
+    ( 'Purchasing'),
+    ( 'Production'),
+    ( 'Marketing');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role ( title, salary, department_id)
 VALUES
-    ('Welder', 62000, 2),
-    ('Worker', 45000, 2),
-    ('Project Manager', 85000, 2),
-    ('Sales Man', 95000, 3),
-    ('Marketing Manager', 125000, 3),
-    ('Accountant', 110000, 1),
-    ('Head of Purchasing', 85000, 1);
+    ( 'Welder', 62000, 2),
+    ( 'Worker', 45000, 2),
+    ( 'Project Manager', 85000, 2),
+    ( 'Sales Man', 95000, 3),
+    ( 'Marketing Manager', 125000, 3),
+    ( 'Accountant', 110000, 1),
+    ( 'Head of Purchasing', 85000, 1);
     
 
-INSERT INTO employees(first_name, last_name, role_id, manager_id) 
+INSERT INTO employee ( first_name, last_name, role_id, manager_id) 
 VALUES
-    ('Ronald', 'McDonald', 1),
-    ('Virginia', 'Driggs', 1),
-    ('Frank', 'Lee', 0),
-    ('Charles', 'Miller', 1),
-    ('Katherine', 'Thegreat', 1),
-    ('Dave', 'Anderson', 0),
-    ('Robert', 'Bell', 0),
-    ('Jessica', 'Sanchez', 1),
-    ('Sarah', 'Nelson', 1),
-    ('Kim', 'George', 1);
+    ( 'Ronald', 'McDonald', 1, 3 ),
+    ( 'Virginia', 'Driggs', 2, 3),
+    ( 'Frank', 'Lee', 3, 5),
+    ( 'Charles', 'Miller', 4, 5),
+    ( 'Katherine', 'Thegreat', 5, NULL),
+    ( 'Dave', 'Anderson', 6, 5 ),
+    ( 'Robert', 'Bell', 7, 5),
+    ( 'Jessica', 'Sanchez', 2, 3),
+    ( 'Sarah', 'Nelson', 2, 3),
+    ( 'Kim', 'George', 1, 3);
