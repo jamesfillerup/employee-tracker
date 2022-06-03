b3768264a3f3fe7313960d11c7a97bf416c9f774
jamesfill <jamesp.fillerup@gmail.com>
2022-06-02 23:47:02 -0600

    department adding to db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
+
 require('dotenv').config();
 const listOptions = ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add a employee', 'Update employee role', 'Escape'];
 
@@ -17,8 +18,6 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the employee database')
 );
-
-
 
 function questionPrompt (){
 
@@ -61,7 +60,7 @@ function questionPrompt (){
         break;
     }
   })
-}
+};
 
 function viewDepartments (){
   console.log('show departments')
@@ -72,25 +71,80 @@ function viewDepartments (){
     console.table(result);
     questionPrompt();
   })
-}
+};
 function viewRoles (){
   console.log('show Roles')
-  
-}
+  questionPrompt();
+};
 function viewEmployees (){
   console.log('show Employees')
-  
-}
+  questionPrompt();
+};
 function addDepartments (){
   console.log('add departments')
   
-}
+  inquirer.prompt(
+    [
+      {
+        type: 'input',
+        name: 'addDepartmentName',
+        message: `Please type the new department name`
+      }      
+    ]
+  )
+  .then((answers) =>{
+    console.table(answers);
+    
+    db.query(`INSERT INTO department (name) 
+    VALUES(?)`,[answers.addDepartmentName],(err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log('department added to database');
+      questionPrompt();
+    })
+  })
+  
+};
 function addRole (){
   console.log('add Role')
   
-}
+  inquirer.prompt(
+    [
+      {
+        type: 'input',
+        name: 'addTitle',
+        message: `Please type the role title`,
+      },
+      {
+        type: 'number',
+        name: 'addSalary',
+        message: `Please type the role salary`,
+      },
+      {
+        type: 'number',
+        name: 'addDepartmentID',
+        message: `Please type the role's department ID`,
+        choices: [1,2,3]
+      }
+    ]
+  )
+  questionPrompt();
+};
 function addEmployee (){
   console.log('add Employee')
+
+  // connection.query("SELECT * FROM role", (err, pickRole) => {
+  //   if (err) throw err;
+  //   const roleChoice = [];
+  //   pickRole.forEach(({ title, id }) => {
+  //     roleChoice.push({
+  //       name: title,
+  //       value: id
+  //     });
+  //   });
+  // });
+
   inquirer.prompt(
     [
       {
@@ -103,30 +157,46 @@ function addEmployee (){
         name: 'addLast',
         message: `What is the employee's last name?`,
       },
-      {
+            {
         type: 'number',
         name: 'roleId',
         message: `What is the employee's role ID?`,
-        choices: inputOptions
+        choices: [1,2,3,4,5,6,7]
       },
+      // {
+      //   type: 'list',
+      //   name: 'roleId',
+      //   message: `What is the employee's role ID?`,
+      //   choices: roleChoice
+      // },
       {
         type: 'number',
-        name: 'options',
+        name: 'managerNumber',
         message: `What is the employee's manager's ID?`,
-        choices: inputOptions
+        choices: [3,5]
       }
     ]
-  );
-}
+  )
+  .then((answers) => {
+    console.table(answers);
+    // connection.query(
+    //     `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?)`
+    // )
+    questionPrompt();
+})
+};
+
+
+
 function updateEmployee (){
   console.log('update employee')
-  
-}
+  questionPrompt();
+};
 function escape (){
   console.log('you have escaped')
   process.exit();
-}
+};
 
 
 
-questionPrompt ();
+questionPrompt();
